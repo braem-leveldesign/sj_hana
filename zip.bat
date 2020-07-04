53d8f7818e7dfc92ed66abff9683a34d8ff9a3ce
brae <brae@braebie.com>
2020-07-04 09:12:12 -0700

    initial commit

---
+++ zip.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\bin\bspzip.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf" -repack -compress "release/sj_hana_rc1.bsp"
